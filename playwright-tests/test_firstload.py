7d6e724a7309d3c57a6c76bab6ef37eb65b0b59c
joshua-mae <86929441+joshua-mae@users.noreply.github.com>
2024-05-29 21:58:37 -0400

    Fixed tests

--- playwright-tests/test_firstload.py
+++ playwright-tests/test_firstload.py
@@ -1,5 +1,25 @@
-from playwright.sync_api import Page, expect
+import asyncio
+from playwright.async_api import async_playwright
 
-def test_has_title(page: Page):
-    page.goto("https://joshuamae.com")
-    expect(page).to_have_title("Joshua Mae")
+async def test_title(url, expected_title):
+    async with async_playwright() as p:
+        browsers = [
+            ("Chromium", p.chromium),
+            ("Firefox", p.firefox),
+            ("WebKit", p.webkit)
+        ]
+        for browser_name, browser_type in browsers:
+            browser = await browser_type.launch()
+            page = await browser.new_page()
+            await page.goto(url)
+            title = await page.title()
+
+            assert title == expected_title, f"{browser_name} - Title does not match. Expected: {expected_title}, but got: {title}"
+
+            await browser.close()
+            print(f"{browser_name} - Test passed")
+
+if __name__ == "__main__":
+    url = "https://joshuamae.com.com"
+    expected_title = "Joshua Mae"
+    asyncio.run(test_title(url, expected_title))
