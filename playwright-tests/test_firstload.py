6d3d8fb8dab81bdb44c495869c628d971b6b22f6
joshua-mae <86929441+joshua-mae@users.noreply.github.com>
2024-05-30 19:08:03 -0400

    Test fix

--- playwright-tests/test_firstload.py
+++ playwright-tests/test_firstload.py
@@ -1,11 +1,12 @@
 import pytest
-from playwright.async_api import async_playwright
+from playwright.async_api import Page, Browser
 
 @pytest.mark.parametrize("browser_name", ["chromium", "firefox", "webkit"])
-async def test_firstload(browser_name, browser_type, context):
-    browser = await browser_type.launch()
-    page = await context.new_page()
-    await page.goto("https://joshuamae.com")
+async def test_firstload(browser_name, browser_type_launch):
+    browser: Browser = await browser_type_launch(browser_name)
+    context = await browser.new_context()
+    page: Page = await context.new_page()
+    await page.goto("https://example.com")
     title = await page.title()
-    assert title == "Joshua Mae", f"Title was '{title}', but expected 'Joshua Mae'"
+    assert title == "Example Domain", f"Title was '{title}', but expected 'Example Domain'"
     await browser.close()
